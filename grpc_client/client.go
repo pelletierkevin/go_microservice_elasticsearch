bf4457c629a099974e2b04e802ac021bc9710bd0
Kevin Pelletier <Kevin.Pelletier@ibm.com>
2021-01-06 15:39:10 +0100

    Update client, remove SayBonjour

--- grpc_client/client.go
+++ grpc_client/client.go
@@ -31,14 +31,7 @@ func main() {
 			log.Fatalf("Error when calling SayHello or SayBonjour: %s", err)
 		}
 		log.Printf("Response from server: %s", response.Body)
-	} else if( modeChoice == "2" ){
-		response, err = c.SayBonjour(context.Background(), &grpc_health.Message{Body: "Hello From Client! I'll wait for your reply!!!"})
-
-		if err != nil {
-			log.Fatalf("Error when calling SayHello or SayBonjour: %s", err)
-		}
-		log.Printf("Response from server: %s", response.Body)
-	} else if ( modeChoice == "3" ) {
+	} else if ( modeChoice == "2" ) {
 		var clusterinfo *grpc_health.ClusterInfo
 		clusterinfo, err = c.GetClusterStatus(context.Background(), &grpc_health.Message{Body: "Asking Cluster status"})
 
@@ -49,7 +42,7 @@ func main() {
 		log.Printf("Cluster name: %s", clusterinfo.Name)
 		log.Printf("Cluster status: %s", clusterinfo.Status)
 		log.Printf("Cluster nb nodes: %s", clusterinfo.Nodes)
-	} else if ( modeChoice == "4" ) {
+	} else if ( modeChoice == "3" ) {
 		indiceName := os.Args[2]
 		var indiceInfo *grpc_health.IndiceInfo
 		indiceInfo, err = c.GetIndiceStatus(context.Background(), &grpc_health.IndiceName{Indicename: indiceName})
@@ -62,7 +55,7 @@ func main() {
 		log.Printf("Indice status: %s", indiceInfo.Status)
 		log.Printf("Indice health: %s", indiceInfo.Health)
 		log.Printf("Indice uuid: %s", indiceInfo.Uuid)
-	} else if ( modeChoice == "5" ) {
+	} else if ( modeChoice == "4" ) {
 		var listIndices *grpc_health.ListIndices
 		listIndices, err = c.GetIndicesList(context.Background(), &grpc_health.Message{Body: "Hello From Client! I'll wait for your reply!!!"})
 
