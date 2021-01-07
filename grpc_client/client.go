22f255810c4cfab3745bf2befa8ea98dfb2a13d1
Kevin Pelletier <Kevin.Pelletier@ibm.com>
2021-01-07 11:39:43 +0100

    Improve grpc client program

--- grpc_client/client.go
+++ grpc_client/client.go
@@ -11,28 +11,32 @@ import (
 
 func main() {
 
-	var conn *grpc.ClientConn
-	conn, err := grpc.Dial("localhost:9000", grpc.WithInsecure())
-	if err != nil {
-		log.Fatalf("Error, did not connect to the gRPC server: %s", err)
-	} else {
-		log.Printf("Connected to gRPC server - Health/Status for Elasticsearch cluster")
-	}
-	defer conn.Close()
+	if (len(os.Args) != 2 || len(os.Args) != 3) {
+		ErrorCommandArguments()
+		return
+    }
 
-	c := grpc_health.NewElasticServiceClient(conn)
+	grpcHostname := "localhost"
+	grpcPort := "9000"
+
+	c, conn := CreateGrpcConnection(grpcHostname, grpcPort)
+	defer conn.Close()
 
 	modeChoice := os.Args[1]
 	var response *grpc_health.Message
 
-	if( modeChoice == "sayhello") {
+	var err error 
+	switch modeChoice {
+
+	case "sayhello":
 		response, err = c.SayHello(context.Background(), &grpc_health.Message{Body: "Hello From Client! I'll wait for your reply!!!"})
 
 		if err != nil {
 			log.Fatalf("Error when calling SayHello or SayBonjour: %s", err)
 		}
 		log.Printf("Response from server: %s", response.Body)
-	} else if ( modeChoice == "clusterhealth" ) {
+	
+	case "clusterhealth":
 		var clusterinfo *grpc_health.ClusterInfo
 		clusterinfo, err = c.GetClusterStatus(context.Background(), &grpc_health.Message{Body: "Asking Cluster status"})
 
@@ -43,7 +47,13 @@ func main() {
 		log.Printf("Cluster name: %s", clusterinfo.Name)
 		log.Printf("Cluster status: %s", clusterinfo.Status)
 		log.Printf("Cluster nb nodes: %s", clusterinfo.Nodes)
-	} else if ( modeChoice == "indicehealth" ) {
+	
+	case "indexhealth":
+		if (len(os.Args) != 3) {
+			ErrorCommandArguments()
+			return
+		} 
+		
 		indiceName := os.Args[2]
 		var indiceInfo *grpc_health.IndiceInfo
 		indiceInfo, err = c.GetIndiceStatus(context.Background(), &grpc_health.IndiceName{Indicename: indiceName})
@@ -56,7 +66,8 @@ func main() {
 		log.Printf("Indice status: %s", indiceInfo.Status)
 		log.Printf("Indice health: %s", indiceInfo.Health)
 		log.Printf("Indice uuid: %s", indiceInfo.Uuid)
-	} else if ( modeChoice == "listindices" ) {
+	
+	case "listindices":
 		var listIndices *grpc_health.ListIndices
 		listIndices, err = c.GetIndicesList(context.Background(), &grpc_health.Message{Body: "Hello From Client! I'll wait for your reply!!!"})
 
@@ -75,21 +86,59 @@ func main() {
 			indiceInfo = listIndices.Indicelist[i]
 			log.Printf("Index [ %s ] - Status : %s - Health : %s - Uuid : %s ", indiceInfo.Indicename, indiceInfo.Status, indiceInfo.Health, indiceInfo.Uuid)
 		}
-
-	} else if ( modeChoice == "createindex" ) {
+	
+	case "createindex":
+		if (len(os.Args) != 3) {
+			ErrorCommandArguments()
+			return
+		} 
 		indiceName := os.Args[2]
 		response, err = c.CreateIndexInCluster(context.Background(), &grpc_health.IndiceName{Indicename: indiceName})
 		if err != nil {
 			log.Fatalf("Create index request response : %s", err)
 		}
 		log.Printf("Create index request succeeded. %s", response.Body)
-	} else if ( modeChoice == "deleteindex" ) {
+
+	case "deleteindex":
+		if (len(os.Args) != 3) {
+			ErrorCommandArguments()
+			return
+		} 
+		
 		indiceName := os.Args[2]
 		response, err = c.DeleteIndexInCluster(context.Background(), &grpc_health.IndiceName{Indicename: indiceName})
 		if err != nil {
 			log.Fatalf("Error when deleting index in Cluster: %s", err)
 		}
 		log.Printf("Delete index request response : %s", response.Body)
-	} 
+	
+	default:
+		ErrorCommandArguments()
+		return
 
+	}
+
+}
+
+func ErrorCommandArguments() {
+	log.Printf("Command error. Please use one of the following commmand :")
+	log.Printf("- %s %s", os.Args[0], "clusterhealth")
+	log.Printf("- %s %s", os.Args[0], "listindices")
+	log.Printf("- %s %s %s", os.Args[0], "indexhealth", "<index name>")
+	log.Printf("- %s %s %s", os.Args[0], "createindex", "<index name>")
+	log.Printf("- %s %s %s", os.Args[0], "deleteindex", "<index name>")
+}
+
+func CreateGrpcConnection(grpcHostname string, grpcPort string) (grpc_health.ElasticServiceClient, *grpc.ClientConn) {
+	var conn *grpc.ClientConn
+	conn, err := grpc.Dial(grpcHostname+":"+grpcPort, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("Error, did not connect to the gRPC server: %s", err)
+	} else {
+		log.Printf("Connected to gRPC server - Health/Status for Elasticsearch cluster")
+	}
+
+	c := grpc_health.NewElasticServiceClient(conn)
+
+	return c, conn
 }
